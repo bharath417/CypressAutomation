601dd0026ded9514983d513d64203f2da232243a
bharath417 <bharath.siddhartha@gmail.com>
2022-04-13 09:45:35 +0530

    initial code commit to branch
    initial code commit to branch

---
+++ cypress/integration/examples/test1.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+describe('My First Test', () =>{
+    it('Does not do much!', () =>{
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //cy.get('.product-action > button').click()
+        //expect(true).to.equal(true)
+        cy.get('.product').should('have.length',5)
+        cy.get('.product:visible').should('have.length',4)
+        //parent child css selctor path to get the visible product
+        cy.get('.products').find('.product').should('have.length',4)
+        cy.get('.products').find('.product').find('.product-name').contains('Carrot').click()
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+    })
+})
