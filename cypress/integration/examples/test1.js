b62e4f7ab8813ee6f5e9f6d958dea26736391b58
bharath417 <bharath.siddhartha@gmail.com>
2022-04-18 14:13:30 +0530

    Update test1.js

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -13,6 +13,7 @@ describe('My First Test', () =>{
         cy.get('.products').find('.product').should('have.length',4)
         cy.get('.products').find('.product').find('.product-name').contains('Carrot').click()
         cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
         cy.get('.products').find('.product').each(($el, index, $list)=> {
             const textVeg = $el.find('h4.product-name').text()
             if (textVeg.includes('Cashews'))
